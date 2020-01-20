2642345324020e28e9a423f8ecd386ed0f0e1c93
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2020-01-20 01:23:41 -0500

    Adds clearSession, Adds comments

--- functional-fishing.php
+++ functional-fishing.php
@@ -1,5 +1,27 @@
 <?php
 
+/**********************************************
+ * STARTER CODE
+ **********************************************/
+
+/**
+ * clearSession
+ * This function will clear the session.
+ */
+function clearSession()
+{
+  session_unset();
+  header("Location: " . $_SERVER['PHP_SELF']);
+}
+
+/**
+ * Invokes the clearSession() function.
+ * This should be used if your session becomes wonky
+ */
+if (isset($_GET['clear'])) {
+  clearSession();
+}
+
 /**
  * getResponse
  * Gets the response history array from the session and converts to a string
@@ -8,7 +30,8 @@
  * 
  * @return string
  */
-function getResponse () {
+function getResponse()
+{
   return implode('<br><br>', $_SESSION['functional_fishing']['response']);
 }
 
@@ -22,10 +45,11 @@ function getResponse () {
  * @param [string] $response
  * @return string
  */
-function updateResponse ($response) {
+function updateResponse($response)
+{
   if (!isset($_SESSION['functional_fishing'])) {
     createGameData();
-  } 
+  }
 
   array_push($_SESSION['functional_fishing']['response'], $response);
 
@@ -38,6 +62,51 @@ function updateResponse ($response) {
  * 
  * @return string
  */
-function help () {
+function help()
+{
   return 'Welcome to Functional Fishing, the text based fishing game. Use the following commands to play the game: <span class="red">eat</span>, <span class="red">fish</span>, <span class="red">fire</span>, <span class="red">wood</span>, <span class="red">bait</span>. To restart the game use the <span class="red">restart</span> command For these instruction again use the <span class="red">help</span> command';
 }
+
+/**********************************************
+ * YOUR CODE BELOW
+ **********************************************/
+
+/**
+ * createGameData
+ * 
+ */
+
+/**
+ * fire
+ *
+ */
+
+/**
+ * bait
+ * 
+ */
+
+/**
+ * wood
+ * 
+ */
+
+/**
+ * fish
+ * 
+ */
+
+/**
+ * eat
+ * 
+ */
+
+/**
+ * inventory
+ * 
+ */
+
+/**
+ * restart
+ * 
+ */
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,18 +8,21 @@
   <title>Functional Fishing with PHP</title>
   <link rel="stylesheet" href="functional-fishing.css">
 </head>
+
 <body>
   <h1>Functional Fishing</h1>
   <div id="response" class="response">
-    <?php if (isset($_SESSION['functional_fishing']['response'])): ?>
+    <?php if (isset($_SESSION['functional_fishing']['response'])) : ?>
       <?php echo getResponse(); ?>
-    <?php else: ?> 
+    <?php else : ?>
       <?php echo updateResponse(help()); ?>
     <?php endif; ?>
   </div>
   <form>
     <input type="text" name="command" class="command" autofocus>
   </form>
+  <a class="clear" href="?clear">Clear Session</a>
   <script src="functional-fishing.js"></script>
 </body>
+
 </html>
